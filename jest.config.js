ef1337efa0112085a257dd3d7b181db52edfccf1
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2021-02-16 01:40:01 -0300

    ➕ Add mongo Jest preset

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+process.env.DEFAULT_SIZE_VALUE = 20;
+process.env.MAX_SIZE_VALUE = 50;
+process.env.MIN_SIZE_VALUE = 10;
+process.env.DEFAULT_PAGE = 1;
+process.env.MIN_PAGE_VALUE = 1;
+
+module.exports = {
+    preset: '@shelf/jest-mongodb',
+};
